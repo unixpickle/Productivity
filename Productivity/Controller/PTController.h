30bef6ce761cbc2b1bb7cc48de05f652d206a1d4
Alex Nichol <alexnichol@comcast.net>
2012-02-18 18:11:31 -0500

    Basic controller implementation

---
+++ Productivity/Controller/PTController.h
@@ -0,0 +1,32 @@
+//
+//  PTController.h
+//  Productivity
+//
+//  Created by Alex Nichol on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PTSession.h"
+#import "PTAppMonitor.h"
+#import "PTPeriodLog.h"
+#import "PTMusicObserver.h"
+
+@interface PTController : NSObject <PTSessionDelegate, PTAppMonitorObserver> {
+    PTSession * currentSession;
+    PTPeriodLog * periodLog;
+    PTAppMonitor * appMonitor;
+    
+    NSMutableArray * stateObservers;
+}
+
+@property (readonly) PTSession * currentSession;
+@property (readonly) PTPeriodLog * periodLog;
+@property (readonly) PTAppMonitor * appMonitor;
+@property (readonly) NSMutableArray * stateObservers;
+
+- (id)initWithLogFile:(NSString *)logFile;
+- (void)beginSession;
+- (void)endSession;
+
+@end
---
+++ Productivity/Controller/PTController.m
@@ -0,0 +1,71 @@
+//
+//  PTController.m
+//  Productivity
+//
+//  Created by Alex Nichol on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PTController.h"
+
+@implementation PTController
+
+@synthesize currentSession;
+@synthesize periodLog;
+@synthesize appMonitor;
+@synthesize stateObservers;
+
+- (id)initWithLogFile:(NSString *)logFile {
+    if ((self = [super init])) {
+        stateObservers = [[NSMutableArray alloc] init];
+        periodLog = [[PTPeriodLog alloc] initWithLogFile:logFile];
+        appMonitor = [[PTAppMonitor alloc] init];
+        if (!periodLog) return nil;
+        
+        PTMusicObserver * musicObserver = [[PTMusicObserver alloc] init];
+        [stateObservers addObject:musicObserver];
+    }
+    return self;
+}
+
+- (void)beginSession {
+    if (currentSession) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Session already running."
+                                     userInfo:nil];
+    }
+    
+    NSArray * observers = [NSArray arrayWithArray:stateObservers];
+    currentSession = [[PTSession alloc] initWithStateObservers:observers];
+    [currentSession setDelegate:self];
+    [currentSession beginSession];
+}
+
+- (void)endSession {
+    if (!currentSession) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Session not running."
+                                     userInfo:nil];
+    }
+    [currentSession endSession];
+    currentSession = nil;
+}
+
+#pragma mark - Session Delegate -
+
+- (void)session:(PTSession *)session finishedPeriod:(PTPeriod *)aPeriod {
+    [periodLog addPeriod:aPeriod];
+}
+
+#pragma mark - App Monitor -
+
+- (void)appMonitor:(PTAppMonitor *)monitor appFocused:(NSUInteger)index {
+    NSString * bundleID = [[monitor applicationBundleIDs] objectAtIndex:index];
+    if ([monitor enabledForBundleID:bundleID]) {
+        [currentSession setIdle:NO];
+    } else {
+        [currentSession setIdle:YES];
+    }
+}
+
+@end
